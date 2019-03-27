8dbbe3eeac7669f6d6f3d250c0fe5e0b5906ed69
rika <rika_4444@yahoo.com>
2019-03-28 02:28:16 +0700

    Get door coordinate

---
+++ main.py
@@ -0,0 +1,37 @@
+arrInput = [
+    '11111111111',
+    '00001000001',
+    '11101011101',
+    '10001010001',
+    '10111010111',
+    '10100010001',
+    '10101010101',
+    '10101010101',
+    '10101010101',
+    '10001010100',
+    '11111111111']
+
+#cek pintu masuk dan keluar
+count  = 0
+door =[]
+i = 0
+row = len(arrInput)
+col = len(arrInput[0])
+while (count<2 and i<row):
+    j = 0
+    while(count<2 and j<col):
+        if(i == 0 and arrInput[i][j] == '0'):
+            door.append([i,j])
+            count+=1
+        elif(j == 0 and arrInput[i][j] == '0'):
+            door.append([i,j])
+            count+=1
+        elif(i == row-1 and arrInput[i][j] == '0'):
+            door.append([i,j])
+            count+=1
+        elif(j == col-1 and arrInput[i][j] == '0'):
+            door.append([i,j])
+            count+=1
+        j+=1
+    i+=1
+
